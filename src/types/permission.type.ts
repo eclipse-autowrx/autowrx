40766b9eb9d0c3a41aeef20c979a55c87679baa1
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-06-12 16:11:40 +0700

    Add permissions object

--- src/types/permission.type.ts
+++ src/types/permission.type.ts
@@ -1,12 +1,3 @@
-export type Permission =
-  | 'createUnlimitedModel'
-  | 'viewModel'
-  | 'updateModel'
-  | 'createPrototype'
-  | 'viewPrototype'
-  | 'updatePrototype'
-  | 'manageUsers'
-
 export interface CheckPermissionResponse {
   hasPermission: boolean
 }
