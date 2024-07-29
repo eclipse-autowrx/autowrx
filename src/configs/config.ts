7a4720324f26c38afa71709dc6dceee7de183651
Tam Thai Hoang Minh <tam.thaihoangminh@vn.bosch.com>
2024-07-29 15:59:01 +0700

    Adjust config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,33 +1,25 @@
 const config: any = {
-  serverBaseUrl:
-    import.meta.env.VITE_SERVER_BASE_URL ||
-    'https://backend-core-dev.digital.auto',
+  serverBaseUrl: 'https://backend-core-covesa.digital.auto',
   serverVersion: import.meta.env.VITE_SERVER_VERSION || 'v2',
-  logBaseUrl: import.meta.env.PROD
-    ? 'https://logs.digital.auto'
-    : 'https://logs.digitalauto.asia',
-  cacheBaseUrl:
-    import.meta.env.VITE_CACHE_BASE_URL || 'https://cache.digitalauto.tech',
   studioUrl: 'https://studio.digital.auto',
   studioBeUrl: 'https://bewebstudio.digitalauto.tech',
   widgetMarketPlaceUrl: 'https://marketplace.digital.auto/packagetype/widget',
   widgetMarketPlaceBe: 'https://store-be.digitalauto.tech',
-  uploadFileUrl: 'https://upload.digitalauto.asia',
+  uploadFileUrl: 'https://upload.digitalauto.tech',
   instanceLogo:
-    'https://static.wixstatic.com/media/604381_7624c1d2f72b45c18183be743e983e45~mv2.png/v1/fill/w_77,h_77,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo-Icon.png',
-  instance: 'digitalauto',
-  defaultModelId: '667a9194694a35002f961ba8',
+    'https://covesa.global/wp-content/uploads/2024/03/covesa_logo.png',
+  instance: 'covesa',
+  defaultModelId: '667d48dde2e9cb0027710f52',
   genAI: {
-    defaultEndpointUrl: '',
+    defaultEndpointUrl: 'https://intermediate.digitalauto.tech/v1/genai',
     marketplaceUrl: 'https://store-be.digitalauto.tech/marketplace/genai',
     sdvApp: [],
     dashboard: [],
     widget: [],
   },
   github: {
-    clientId: import.meta.env.VITE_GITHUB_CLIENT_ID || '',
+    clientId: 'Ov23livVYo2MXyqoIHox',
   },
-  features: [],
 }
 
 export default config
