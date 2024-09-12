f2941e22a0a4ff197f9087b77f0558db937dc692
Tam Thai Hoang Minh <tam.thaihoangminh@vn.bosch.com>
2024-09-12 14:18:58 +0700

    Implement real runtime simulate for genai wizard

--- src/components/molecules/genAI/DaGenAI_Simulate.tsx
+++ src/components/molecules/genAI/DaGenAI_Simulate.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import useWizardGenAIStore from '@/stores/genAIWizardStore'
 import DaGenAI_SimulateDashboard from './DaGenAI_SimulateDashboard'
+import DaGenAI_RuntimeControl from './DaGenAI_RuntimeControl'
 
 type DaGenAI_SimulateProps = {}
 
@@ -22,6 +23,7 @@ const DaGenAI_Simulate = ({}: DaGenAI_SimulateProps) => {
   return (
     <div className="flex h-full w-full flex-col py-2  ">
       <DaGenAI_SimulateDashboard />
+      <DaGenAI_RuntimeControl />
     </div>
   )
 }
