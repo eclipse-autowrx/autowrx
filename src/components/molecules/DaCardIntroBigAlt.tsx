25a26d445ea2de7bb76eb3732ac553a8cd92ecea
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-09-12 13:59:28 +0700

    fix small UI issues in home page

--- src/components/molecules/DaCardIntroBigAlt.tsx
+++ src/components/molecules/DaCardIntroBigAlt.tsx
@@ -15,7 +15,7 @@ const DaCardIntroBig = ({ title, content, url }: CardIntroProps) => {
     <Link
       to={url || '#'}
       className={clsx(
-        'flex flex-col min-h-28 w-full bg-da-white rounded-lg border p-4 select-none',
+        'flex flex-col min-h-28 w-full h-full bg-da-white rounded-lg border p-4 select-none',
         url ? 'hover:border-da-primary-300' : 'pointer-events-none',
       )}
     >
