72f34f91a964df57dca6e0daf765e05807206d0a
7inh-bosch <til7hc@bosch.com>
2026-07-20 15:52:23 +0700

    fix prototype size

--- frontend/src/components/molecules/DaPrototypeItem.tsx
+++ frontend/src/components/molecules/DaPrototypeItem.tsx
@@ -306,6 +306,7 @@ const DaPrototypeItem = ({ prototype, className }: DaPrototypeItemProps) => {
           e.stopPropagation()
         }
       }}
+      className='size-full'
     >
       {enableContextMenu && isOwner ? (
         <ContextMenu>
